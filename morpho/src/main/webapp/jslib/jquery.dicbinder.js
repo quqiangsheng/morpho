e3d5013983fabe4fb8b8d340857e0c7db2a85177
fbf <help@max256.com@c48f2573-13e7-43e4-a753-fcbb2e61a85e>
2017-06-06 01:59:22 +0000

    1更新前端js数据字典绑定插件

--- morpho/src/main/webapp/jslib/jquery.dicbinder.js
+++ morpho/src/main/webapp/jslib/jquery.dicbinder.js
@@ -1,43 +1,91 @@
 /**
- * Data Dictionary Binder
- * 
- * 
- * 
+ * Data Dictionary Binder数据字典绑定工具
  */
 (function($) {
 	$.extend({
-		dicbinder : function(dictionary, code, queryParams) {
+		//定义方法 获得数据字典 code 字典value   需要遍历的dictionary字典数组
+		getSysCode : function(dictionary, code,url,method) {
 			var dic = $(document).data(dictionary);
+			//加载dic如果为空则从服务器加载 不为空遍历查找
 			if (dic) {
-				return dic[code];
+				//遍历dic
+				for(item in dic){
+					//CODE_NAME是专属字典表属性名如果有值则说明是sys_code表的数据 
+					if(item.codeName==dictionary&&item.codeValue==code){
+						return item.codeText;
+					}
+				}
+				//如果没有找到返回原函数值
+				return code;
 			}
-			$.loaddictionary(dictionary, queryParams);
+			//从服务器加载
+			$.loaddictionary(dictionary,url,method);
 			dic = $(document).data(dictionary);
+			//
 			if (dic) {
-				return dic[code];
+				//遍历dic
+				for(item in dic){
+					//CODE_NAME是专属字典表属性名如果有值则说明是sys_code表的数据 
+					if(item.codeName==dictionary&&item.codeValue==code){
+						return item.codeText;
+					}
+				}
+				//如果没有找到返回原函数值
+				return code;
 			}
-			return code;
 		}
 	});
-	$.loaddictionary = function(dictionary, queryParams) {
+	$.extend({
+		//定义方法 获得数据字典数组  自定义数据结构自行遍历
+		getSysCodeArray : function(dictionary, code,url,method) {
+			var dic = $(document).data(dictionary);
+			//加载dic如果为空则从服务器加载 
+			if (dic) {
+				return dic;
+			}
+			//从服务器加载
+			$.loaddictionary(dictionary,url,method);
+			dic = $(document).data(dictionary);
+			//
+			if (dic) {
+				return dic;
+			}else{
+				return null;
+			}
+		}
+	});
+	//初始化页面时执行下面这段js 向服务器加在需要的字典列表 并缓存在客户端
+	//dictionary字典名字   url请求url  请求方式method ‘get’或者‘post’
+	$.loaddictionary = function(dictionary, url,method) {
+		//数据缓存dictionary存储的数据名
 		var dic = $(document).data(dictionary);
+		//dic存储的value是[]数组
 		if (!dic) {
-			var param = $.extend({}, queryParams || {});
+			//没有数据的话 向服务器加载数据  
+			var param = {
+				pageNum:1,
+				pageSize:100000,
+				codeName:dictionary//codeName是需要查找的字典名称 如果为空则返回全部字典 前边的分页参数默认取前10w条 实际不可能达到这已经很大了 具体要根据服务器端实现
+			};
 			$.ajax({
-				type : 'get',
-				url : 'backstage/code/' + dictionary,
+				type : method,
+				url : url ,
 				data : param,
 				dataType : 'json',
+				async:false,
 				success : function(data) {
-					dic = {};
-					for (var i = 0; i < data.length; i++) {
-						var v = data[i]['Value'] + '';
-						var s = data[i]['Text'];
-						dic[v] = s;
+					if(data.status=='1'){
+						//如果成功
+						dic = [];//数组
+						dic =data.rows;//缓存
+						$(document).data(dictionary, dic);
+					}else{
+						console.warn("加载数据字典时失败！返回信息"+data.info);
 					}
-					$(document).data(dictionary, dic);
+					
 				},
 				error : function() {
+					console.warn("加载数据字典时失败！网络错误");
 				}
 			});
 		}
