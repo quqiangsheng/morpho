255537092ceffef4b3776d652d1ffc058cdeca54
312182539@qq.com <312182539@qq.com>
2017-06-08 03:28:25 +0000

    1增加了http接口登陆的处理方法 不进行重定向 json格式返回,并增加了跨域处理

--- morpho/src/main/java/com/max256/morpho/common/security/shiro/filter/MyFormAuthenticationFilter.java
+++ morpho/src/main/java/com/max256/morpho/common/security/shiro/filter/MyFormAuthenticationFilter.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -24,8 +25,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.max256.morpho.common.config.Constants;
+import com.max256.morpho.common.dto.GeneralReturnData;
+import com.max256.morpho.common.dto.R;
 import com.max256.morpho.common.entity.SysLog;
 import com.max256.morpho.common.util.IPUtils;
+import com.max256.morpho.common.util.JsonUtils;
 import com.max256.morpho.common.util.UUIDUtils;
 import com.max256.morpho.sys.service.SysLogService;
 
@@ -66,8 +70,25 @@ public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
 			log.info("登录日志:"+sysLog);
 		}
 		sysLogService.insert(sysLog);
+		//判断请求方式 如果是ajax或者http接口形式的登陆 返回json 不重定向
+		Boolean isAjaxFlag=isAjax((HttpServletRequest)request, (HttpServletResponse)response);
+		if(isAjaxFlag){
+			//返回json信息
+			GeneralReturnData<String> returnData= R.data();
+			returnData.setStatus("1");
+			returnData.setInfo("登陆成功");
+			String returnJsonData=JsonUtils.writeValueAsString(returnData);
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("application/json;charset=UTF-8");
+			//允许跨域
+			((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+			response.getWriter().println(returnJsonData);
+			return false;
+		}else{
+			return super.onLoginSuccess(token, subject, request, response);
+		}
 		//we handled the success redirect directly, prevent the chain from continuing:
-		return super.onLoginSuccess(token, subject, request, response);
+		
 	}
 	
 	
@@ -93,16 +114,62 @@ public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
 		// 带参数转发
 		Map<String,String> param=new HashMap<String,String>();
 		param.put(Constants.LOGIN_FAILURE_KEY, error);
-        try {
-			WebUtils.issueRedirect(request, response, Constants.ERROR_PAGE_URL,param);
-		} catch (IOException e1) {
+ 
+        //判断请求方式 如果是ajax或者http接口形式的登陆 返回json 不重定向
+  		Boolean isAjaxFlag=isAjax((HttpServletRequest)request, (HttpServletResponse)response);
+  		if(isAjaxFlag){
+  			//返回json信息
+  			GeneralReturnData<String> returnData= R.data();
+  			returnData.setStatus("0");
+  			returnData.setInfo("登陆失败,错误代码:"+error);
+  			String returnJsonData=JsonUtils.writeValueAsString(returnData);
+  			try {
+  				response.setCharacterEncoding("UTF-8");
+  				response.setContentType("application/json;charset=UTF-8");
+  				//允许跨域
+  				((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+				response.getWriter().println(returnJsonData);
+				
+			} catch (IOException e1) {
+			}
+  			return false;
+  		}else{
+  			try {
+  				WebUtils.issueRedirect(request, response, Constants.ERROR_PAGE_URL,param);
+  			} catch (IOException e1) {
 
-		}
-		//停止链式处理 本步骤已经重定向
-        return false;
+  			}
+  			//停止链式处理 本步骤已经重定向
+  	        return false;
+  		}
+		
 		
 
 	}
+	
+	/**
+	 * 判断当前请求是否为异步请求.
+	 * 综合两种判断方法
+	 * 1.根据客户端的http请求头判断
+	 * 2.根据访问controller是否有ResponseBody注解判断
+	 */
+	private boolean isAjax(HttpServletRequest request, HttpServletResponse response){
+		//判断请求头
+		String flagHeader=request.getHeader("X-Requested-With");
+		if (flagHeader != null && !flagHeader.equals("") && !flagHeader.equals("null")) {
+			return (true);
+		}
+		String acceptHeader=request.getHeader("Accept");
+		if (acceptHeader != null 
+				&& !acceptHeader.equals("")
+				&& !acceptHeader.equals("null")
+				//表明带json字符
+				&&acceptHeader.toLowerCase().indexOf("json")!=-1) {
+			return (true);
+		}
+		return (false);
+	}
+	
 
 
 }
