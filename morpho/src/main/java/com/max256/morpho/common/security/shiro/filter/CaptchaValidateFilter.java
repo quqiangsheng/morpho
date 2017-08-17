d4a7e26c6366bad136f29c2d3d2330cdb638bc65
fbf <312182539@qq.com>
2017-08-17 10:13:51 +0000

    1修复ajax提交时如果开启验证码验证的状态下返回的不是json标准结构而是重定向http状态码的情况

--- morpho/src/main/java/com/max256/morpho/common/security/shiro/filter/CaptchaValidateFilter.java
+++ morpho/src/main/java/com/max256/morpho/common/security/shiro/filter/CaptchaValidateFilter.java
@@ -6,12 +6,17 @@ import java.util.Map;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.web.filter.AccessControlFilter;
 import org.apache.shiro.web.util.WebUtils;
 
 import com.max256.morpho.common.config.Constants;
+import com.max256.morpho.common.dto.GeneralReturnData;
+import com.max256.morpho.common.dto.R;
+import com.max256.morpho.common.util.AjaxUtils;
 import com.max256.morpho.common.util.CaptchaUtils;
+import com.max256.morpho.common.util.JsonUtils;
 
 /**
  * 验证码验证过滤器 用于form表单 post方式的验证码验证 shiro filter扩展 如果使用注意：
@@ -62,10 +67,25 @@ public class CaptchaValidateFilter extends AccessControlFilter {
 	@Override
 	protected boolean onAccessDenied(ServletRequest request,
 			ServletResponse response) throws Exception {
-		// 带参数转发
-        Map<String,String> param=new HashMap<String,String>();
-		param.put(Constants.LOGIN_FAILURE_KEY, Constants.CAPTCHA_FAILURE);
-		WebUtils.issueRedirect(request, response, Constants.ERROR_PAGE_URL,param);
+		if(AjaxUtils.isAjax((HttpServletRequest)request, (HttpServletResponse)response)){
+			//直接输出json结果 不再转发
+			GeneralReturnData<String> returnData= R.data();
+			returnData.setStatus("0");
+			returnData.setInfo("登陆失败,验证码错误");
+			String returnJsonData=JsonUtils.writeValueAsString(returnData);
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("application/json;charset=UTF-8");
+			//允许跨域
+			((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+			response.getWriter().println(returnJsonData);
+			
+		}else{
+			//带参数转发
+	        Map<String,String> param=new HashMap<String,String>();
+			param.put(Constants.LOGIN_FAILURE_KEY, Constants.CAPTCHA_FAILURE);
+			WebUtils.issueRedirect(request, response, Constants.ERROR_PAGE_URL,param);
+		}
+		
         //停止链式处理 本步骤已经重定向
         return false;
 	}
