87d9d6596b636837ba2c56a6158dffcc70065fd0
fbf <312182539@qq.com@c48f2573-13e7-43e4-a753-fcbb2e61a85e>
2017-08-18 15:20:34 +0000

    删除ocr依赖后忘记删除测试用例 删除

--- morpho/src/main/java/com/max256/morpho/common/test/Test.java
+++ morpho/src/main/java/com/max256/morpho/common/test/Test.java
@@ -13,7 +13,6 @@ import org.jsoup.Connection.Response;
 import org.apache.commons.lang.StringUtils;
 import org.jsoup.Jsoup;
 
-import com.asprise.ocr.Ocr;
 import com.max256.morpho.common.config.HttpStatus;
 
 public class Test {
@@ -102,81 +101,5 @@ public class Test {
 
 	}*/
 	
-	public static void main(String[] args)throws IOException {
-		/*test1134();*/
-		String name="com.max256.SysUser";
-		StringBuilder builder = new StringBuilder(name.replace('.', '_'));
-		for (int i = 1; i < builder.length() - 1; i++) {
-			if (isUnderscoreRequired(builder.charAt(i - 1), builder.charAt(i),
-					builder.charAt(i + 1))) {
-				builder.insert(i++, '_');
-			}
-		}
-		System.out.println(builder.toString());
-	}
-	private static boolean isUnderscoreRequired(char before, char current, char after) {
-		return Character.isLowerCase(before) && Character.isUpperCase(current)
-				&& Character.isLowerCase(after);
-	}
-	public static void test11(){
-		
-	}
-	public static void test113(){
-		//测试httpStatus
-		System.out.println(HttpStatus.BAD_GATEWAY);
-	}
-	public static void test1134() throws IOException{
-		String code=null;
-		//测试爬虫的验证码模拟登录
-		//登录主页 获取cookie
-		Map<String, String> cookies = null;
-		Response res = Jsoup.connect("http://localhost:8080/morpho/login").method(Method.GET).timeout(30000).execute();
-		cookies = res.cookies();
-		System.out.println(cookies);
-		
-		for(;;){
-			if(StringUtils.isBlank(code)){
-				//带cookie获取验证码
-				Response resCaptcha = Jsoup.connect("http://localhost:8080/morpho/captcha.jpg").ignoreContentType(true).cookies(cookies).method(Method.GET).timeout(30000).execute();
-				//图片的字节数组
-				byte[] captcha=resCaptcha.bodyAsBytes();
-				ByteArrayInputStream in = new ByteArrayInputStream(captcha); //将b作为输入流；
-				BufferedImage image = ImageIO.read(in); //将in作为输入流，读取图片存入image中，而这里in可以为ByteArrayInputStream();
-				//识别验证码
-				//尝试多次识别
-				code=recognize(image);
-			}else{
-				break;
-			}
-			
-		}
-		
-		//空的话
-		//请求参数
-		Map<String, String> map = new HashMap<String, String>();  
-		map.put("uname", "admin");  
-		map.put("upass", "admin");  
-		map.put("captcha", code);  
-		//识别的验证码+用户名密码登录
-		Response res2 = Jsoup.connect("http://localhost:8080/morpho/login").
-				data(map).method(Method.POST).timeout(30000).execute();
-		
-		//调用其他api
-		if(res2.body().contains("系统验证码错误")){
-			test1134();
-		}else{
-			System.out.println(res2.body());
-		}
-		
-	}
-
-	public static String recognize(BufferedImage image) {
-        Ocr.setUp();
-        Ocr ocr = new Ocr();
-        ocr.startEngine("eng", Ocr.SPEED_SLOW);
-        String result = ocr.recognize(image,com.asprise.ocr.Ocr.RECOGNIZE_TYPE_ALL, com.asprise.ocr.Ocr.OUTPUT_FORMAT_PLAINTEXT,0,null);
-        ocr.stopEngine();
-        System.out.println("识别结果:"+result);
-        return result;
-	}
+	
 }
