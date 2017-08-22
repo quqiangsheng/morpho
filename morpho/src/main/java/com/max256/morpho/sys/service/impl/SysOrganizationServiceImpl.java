83d51167b4f1325accb77401d92d858007e72e3d
fbf <312182539@qq.com@c48f2573-13e7-43e4-a753-fcbb2e61a85e>
2017-08-22 15:34:55 +0000

    1完善组织机构和资源权限的递归删除 使用程序实现 避免使用数据库特有语句

--- morpho/src/main/java/com/max256/morpho/sys/service/impl/SysOrganizationServiceImpl.java
+++ morpho/src/main/java/com/max256/morpho/sys/service/impl/SysOrganizationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.max256.morpho.sys.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.max256.morpho.common.entity.SysOrganization;
@@ -10,13 +12,21 @@ import tk.mybatis.mapper.entity.Example;
 @Service("sysOrganizationService")
 public class SysOrganizationServiceImpl extends BaseServiceImpl<SysOrganization> implements SysOrganizationService{
 	@Override
-	public void deleteSelfAndSubSysOrganization(SysOrganization sysOrganization){
+	public void deleteSelfAndSubSysOrganization(SysOrganization sysOrganization){		
 		//先删除自己
 		this.delete(sysOrganization);
-		//删除子
+		//判断有没有子
 		Example example =new Example(SysOrganization.class);
 		example.createCriteria().andEqualTo("parentId", sysOrganization.getOrgId());
-		//删除
-		this.deleteByExample(example);
+		//如果有子 则selectByExample.size()大于0
+		List<SysOrganization> selectByExample = this.selectByExample(example);
+		if(selectByExample!=null&&selectByExample.size()>0){
+			//递归删除
+			for (SysOrganization sysOrganization2 : selectByExample) {
+				deleteSelfAndSubSysOrganization(sysOrganization2);
+			}
+		}
+		
+		
 	}
 }
