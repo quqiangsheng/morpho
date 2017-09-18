d82cf14a0fa4bf0bbd149d6d50b2dc00b2525a61
fbf <312182539@qq.com>
2017-09-18 02:20:14 +0000

    1新增返回前台json包装功能 可以加入类似数据字典翻译之类的特性 按需取用 提供了除ajax获取字典值的另一种可选项

--- morpho/src/main/java/com/max256/morpho/common/dto/R.java
+++ morpho/src/main/java/com/max256/morpho/common/dto/R.java
@@ -1,6 +1,13 @@
 package com.max256.morpho.common.dto;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.beanutils.BeanUtils;
 
 import com.max256.morpho.common.exception.BusinessException;
 import com.max256.morpho.common.exception.ParameterException;
@@ -115,5 +122,119 @@ public class R implements Serializable {
 	public static void paramException(String message,int code) throws ParameterException{
 		throw new ParameterException(message,code);
 	}
+	
+	/**
+	 * 包装返回数据 可以封装一些字典项对照内容或者其他您需要的内容
+	 * @param returnData
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static GeneralReturnData wrapList(GeneralReturnData returnData,WrapCommand wrapCommand)  {
+		if(returnData==null){
+			new ParameterException("returnData不能为null");
+		}
+		if(wrapCommand==null){
+			new ParameterException("wrapCommand不能为null");
+		}
+		//判断GeneralReturnData类型
+		if(returnData instanceof TreeReturnData){
+			//TreeReturnData不包装直接返回
+			return returnData;
 
+		}else if(returnData instanceof ListPageReturnData){
+			List<HashMap> result=new ArrayList();
+			ListPageReturnData listPageReturnData=(ListPageReturnData)returnData;
+			List list = listPageReturnData.getList();
+			//为空判断
+			if(list==null||list.size()==0){
+				//不处理
+				return returnData;
+			}
+			for (Object item : list) {
+				HashMap describe = null;
+				try {
+					describe = (HashMap) BeanUtils.describe(item);
+				} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+					//包装失败 返回原对象
+					return returnData;
+				}
+				HashMap describe1 = null;
+				try {
+					describe1 = (HashMap) BeanUtils.cloneBean(describe);
+				} catch (IllegalAccessException | InstantiationException | InvocationTargetException
+						| NoSuchMethodException e) {
+					//包装失败 返回原对象
+					return returnData;
+				}//返回的单个bean的加上属性后的map
+				Set<String> keySet = describe.keySet();//bean propertity key
+				for (String object : keySet) {
+					describe1.put(object, describe.get(object));
+					if(object.equals("class")){
+						//class跳过
+					}else{
+						//其他调用用户命令处理
+						wrapCommand.wrap(object, describe.get(object), describe1);
+					}
+				}
+				result.add(describe1);
+			}
+			//设置返回数据
+			listPageReturnData.setList(result);
+			
+		}else if(returnData instanceof DataGridReturnData){
+			List<HashMap> result=new ArrayList();
+			DataGridReturnData dataGridReturnData=(DataGridReturnData)returnData;
+			List list = dataGridReturnData.getRows();
+			//为空判断
+			if(list==null||list.size()==0){
+				//不处理
+				return returnData;
+			}
+			for (Object item : list) {
+				HashMap describe = null;
+				try {
+					describe = (HashMap) BeanUtils.describe(item);
+				} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+					//包装失败 返回原对象
+					return returnData;
+				}
+				HashMap describe1 = null;
+				try {
+					describe1 = (HashMap) BeanUtils.cloneBean(describe);
+				} catch (IllegalAccessException | InstantiationException | InvocationTargetException
+						| NoSuchMethodException e) {
+					//包装失败 返回原对象
+					return returnData;
+				}//返回的单个bean的加上属性后的map
+				Set<String> keySet = describe.keySet();//bean propertity key
+				for (String object : keySet) {
+					describe1.put(object, describe.get(object));
+					if(object.equals("class")){
+						//class跳过
+					}else{
+						//其他调用用户命令处理
+						wrapCommand.wrap(object, describe.get(object), describe1);
+					}
+				}
+				result.add(describe1);
+			}
+			//设置返回数据
+			dataGridReturnData.setRows(result);
+			return dataGridReturnData;
+		}else{
+			//本身的类型 不包装直接返回
+			return returnData;
+		}
+		return returnData;
+		
+	}
+	//带有抽象方法的内部函数式接口 用于实现操纵包装对象的命令
+	@FunctionalInterface
+	public interface  WrapCommand{
+		public  void wrap(String fieldName,Object fildValue,HashMap<String,Object> wrapHashMap);
+	}
+	
 }
