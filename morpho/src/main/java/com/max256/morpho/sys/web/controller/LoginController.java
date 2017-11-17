322846ec16784385ba002b4b049fef36e30f9914
fbf <312182539@qq.com@c48f2573-13e7-43e4-a753-fcbb2e61a85e>
2017-11-17 08:24:34 +0000

    1更新jar到最新版20171117 2打开druid多数据源在一个页面监控 3修正uflo的properties设置

--- morpho/src/main/java/com/max256/morpho/sys/web/controller/LoginController.java
+++ morpho/src/main/java/com/max256/morpho/sys/web/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.max256.morpho.sys.web.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
